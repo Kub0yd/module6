b4af46c0f1729f4bfd24013d7ef711c1fac86658
Kub0yd <vs_egorov@mail.ru>
2022-11-04 23:47:31 +0300

    add styles

--- task4/script.js
+++ task4/script.js
@@ -2,6 +2,15 @@ const link = document.querySelector('a');
 
 link.addEventListener('click', (event) => {
     PromptText = prompt('Введите новый текст для ссылки:', link.textContent);
+    //If 'Cancel' pressed, break out of the function early
+    if (PromptText === null){
+        return;
+    }
+    if (PromptText != ""){
     link.textContent = PromptText;
     event.preventDefault();
+    } else {
+        alert('Значение не может быть пустым!');
+    }
+    
 })
